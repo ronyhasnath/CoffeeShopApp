df03376d75844fd99e9122953b0f0dc68eb10b2c
RonyHasnath <rony.hasnath@jato.com>
2020-03-09 13:20:57 -0400

    created

---
+++ ConsoleApp_Rony_CoffeeShop/ConsoleApp_Rony_CoffeeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_Rony_CoffeeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
